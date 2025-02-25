501a611a07cc3528bf33779632252b886fb0b611
FM <fmotaharinejadf@gmail.com>
2025-02-25 22:12:37 +0330

    Countdown timer

---
+++ src/Timer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './style.css'
+
+
+var interval;
+
+
+class Timer extends React.Component{  //component
+    constructor(){
+      super();
+      this.state = {
+        time: 10
+      }
+    }
+
+    componentDidMount(){  //یک بار اول کار میسازه
+        interval = setInterval(()=>{ //interval برای تغییر state
+            this.setState({
+              time: this.state.time - 1
+            })
+          }, 1000)
+    }
+
+    componentDidUpdate(){
+        if (this.state.time == 0){  //وقتی به این زمان رسید تایمر متوقف میشود
+            clearInterval(interval)
+        }
+    }
+
+    componentWillUnmount(){
+        
+    }
+  
+    render(){
+      return(
+        <h2 className='timer'>
+          {this.state.time}
+        </h2>
+      )
+    }
+  }
+
+export default Timer;
