fc0fc0bb16de0a7622ee8dfef6542a3ab19e94b2
FM <fmotaharinejadf@gmail.com>
2025-02-25 22:27:55 +0330

    #12_props

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ import Timer from './Timer'
 
 
 class App extends React.Component{  //component
-    render(){
-      return(
-        // * class --> className
-        // حتما باید یک تگ باز و بسته باشد. میتواند تگ بی نام باشد
-        <div className='main'>  
-          <Text/> 
-          <Timer/>
-        </div>
-  
-        // <>    با بالا برابر است
-        //   <Text/>
-        //   <Timer/>
-        // </>
-      )
+  constructor(){
+    super();
+    this.state = {
+      title: 'hello counter...'
     }
   }
+
+  render(){
+    return(
+      // * class --> className
+      // حتما باید یک تگ باز و بسته باشد. میتواند تگ بی نام باشد
+      <div className='main'>  
+        <Text title = {this.state.title}/> 
+        <Timer/>
+      </div>
+
+      // <>    با بالا برابر است
+      //   <Text/>
+      //   <Timer/>
+      // </>
+    )
+  }
+}
 
 export default App;
--- src/Text.js
+++ src/Text.js
@@ -4,8 +4,11 @@ import ReactDOM from 'react-dom/client';
 
 class Text extends React.Component{  //component
     render(){
+      console.log(this.props)  //output: title: "hello counter..."
+      
+
       return(
-        <h1>hello countdown timer</h1>
+        <h1>{this.props.title}</h1>
       )
     }
   }
